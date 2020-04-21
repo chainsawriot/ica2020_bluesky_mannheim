9bd9fdaac7c132ad3e58f419996b4bde0431933e
chainsawriot <chainsawtiney@gmail.com>
2020-04-21 11:57:20 +0200

    adjust typing speed

--- render.R
+++ render.R
@@ -8,8 +8,8 @@ junk <- record("typing.R", process = process, typing_speed = .0000000001)
 sdc <- record("textsdc.R", process = process, typing_speed = 0.1)
 write_svg(sdc, path = "sdc.svg", window = FALSE, cursor = FALSE)
 
-##plex <- record("textplex.R", typing_speed = .000001)
-##write_svg(plex, path = "plex.svg", window = FALSE, cursor = FALSE)
+plex <- record("textplex.R", typing_speed = .1)
+write_svg(plex, path = "plex.svg", window = FALSE, cursor = FALSE)
 
 resdtmf <- record("resdtmf.R", process = process, typing_speed = 0.1)
 write_svg(resdtmf, path = "resdtmf.svg", window = FALSE, cursor = FALSE)
